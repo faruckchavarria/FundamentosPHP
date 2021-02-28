6f352568b1c78a77032185828f60ef8e1ccdbbab
faruckchavarria <faruckchavarria2@gmail.com>
2021-02-28 17:32:16 -0600

    Se agrega las condicionales para la compromacion de edad y la ruta de los datos en el html

--- Practica1/Ejercicio1/index.php
+++ Practica1/Ejercicio1/index.php
@@ -6,8 +6,19 @@ $Anio = $Fecha[0];
 $Anio1 = $Fecha[1];
 $Anio2 = $Fecha[2];
 $Anio3 = $Fecha[3];
+//realizamos comprobacionde cada digito por el año en que nacio.
 $Nace = $Anio.$Anio1.$Anio2.$Anio3;
+//necesitamos especificar el año actual para hacer la comprobacion
 $anio_actual = 2021;
+//comprobamos el año ingresado por el año actaul
 $Edad = ($anio_actual - $Nace);
+//se agrega la condiciones para la valoracion de edad en el acceso
+if ($Edad >= 18)
+{
+    echo ("Bienvenido al sitio " . $Nombre);
+} else {
+    echo ("" . $Nombre. "  no puedes entrar por ser menor de edad");
+}
+
 
 ?>
